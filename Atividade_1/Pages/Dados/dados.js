1c4756d8b74e5e8a449046f61d7e3096ff56f9a2
Guilherme-Cavazzini <guilhermescavazzini@outlook.com>
2025-03-03 01:21:19 -0400

    arquiteturas das pages, e logica dos dados ainda incompleta

--- Atividade_1/Pages/Dados/dados.js
+++ Atividade_1/Pages/Dados/dados.js
@@ -3,6 +3,11 @@ let ctx1 = canvas1.getContext("2d");
 let canvas2 = document.getElementById("dados2");
 let ctx2 = canvas2.getContext("2d");
 
+var ganhou = 0;
+var perdeu = 0;
+var first = true;
+var numerossorteados = [];
+
 function limparCanvas(ctx, canvas) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
@@ -60,6 +65,58 @@ function sorteio() {
 
   desenharPontos(ctx1, pontosDados[sorteio1]);
   desenharPontos(ctx2, pontosDados[sorteio2]);
+
+  sum = sorteio1 + sorteio2;
+
+  console.log("Numeros sorteados" + numerossorteados);
+
+  if (first == true) {
+    result(sum);
+    console.log(sum + " first");
+    first = false;
+  } else {
+    resultsecond(sum);
+    console.log(sum + " second");
+  }
+}
+
+function result(sum) {
+  switch (sum) {
+    case 7:
+    case 11:
+      Vitorias();
+      break;
+    case 2:
+    case 3:
+    case 12:
+      Derrotas();
+      break;
+  }
+}
+
+function resultsecond(sum) {
+  if (numerossorteados.includes(sum)) {
+    Vitorias();
+    console.log(numerossorteados);
+  }
+  switch (sum) {
+    case 7:
+      Derrotas();
+      console.log(numerossorteados);
+      first = true;
+      break;
+  }
+}
+
+function Vitorias() {
+  ganhou++;
+  console.log(numerossorteados);
+  document.getElementById("ganhou").innerHTML = ganhou;
+}
+
+function Derrotas() {
+  perdeu++;
+  document.getElementById("perdeu").innerHTML = perdeu;
 }
 
 window.onload = function () {
